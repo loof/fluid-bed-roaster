bf8329d83faee0dd12f2bc6750a2a911664c79b5
loof <ypeissard@gmail.com>
2024-01-26 18:57:23 +0100

    standard tc4+ config

---
+++ src/main.h
@@ -0,0 +1,11 @@
+//
+// Created by Yves Peissard on 26/01/2024.
+//
+
+#ifndef FLUID_BED_ROASTER_MAIN_H
+#define FLUID_BED_ROASTER_MAIN_H
+
+float convertUnits(float t);
+void getProfileDescription(int pn);
+
+#endif //FLUID_BED_ROASTER_MAIN_H
